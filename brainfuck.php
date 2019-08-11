94a61ea1aa64e5c6a3f7717ec45c33ace8e46f52
Marush Denchev <avreon@gmail.com>
2019-08-11 09:23:51 +0300

    Tidy up the white spacing

--- brainfuck.php
+++ brainfuck.php
@@ -17,87 +17,64 @@ class BrainFuck {
 	private $input_pointer = 0;
 
 	public function __construct( $code, $input = "" ) {
-
 		$this->code = preg_replace( '/[^<>+\-\.,\[\]]/', '', $code );
-
 		$this->input = $this->split( $input );
 	}
 
 	public function compile() {
+		$this->chars = $this->split($this->code);
 
-		$this->chars = $this->split( $this->code );
-
-		for( $i = 0, $n = count( $this->chars ); $i < $n; $i += 1 ) {
-
+		for ($i = 0, $n = count( $this->chars ); $i < $n; $i += 1) {
 			$char = $this->chars[ $i ];
 
-			switch( $char ) {
-
+			switch ($char) {
 				// Increment byte
-				case '+' :
-
+				case '+':
 					$this->increment_byte();
-
 				break;
 
 				// Decrement byte
-				case '-' :
-
+				case '-':
 					$this->decrement_byte();
-
 				break;
 
 				// Increment data pointer
-				case '>' :
-
+				case '>':
 					$this->increment_pointer();
-
 				break;
 
 				// Decrement data pointer
-				case '<' :
-
+				case '<':
 					$this->decrement_pointer();
-
 				break;
 
 				// Get current data pointer cell and at it to output
-				case '.' :
-
-					if( $this->get_byte() == 0 )
+				case '.':
+					if ($this->get_byte() == 0) {
 						return $this->output;
+					}
 
 					$ascii = $this->byte_to_ascii();
-
 					$this->output .= $ascii;
-
 				break;
 
 				// Begin loop
-				case '[' :
-
-					if( $this->get_byte() == 0 ) {
-
+				case '[':
+					if ($this->get_byte() == 0) {
 						$i = $this->loop_end_position($i);
 					}
-
 				break;
 
 				// End loop
-				case ']' ;
-
-					if( $this->get_byte() != 0 ) {
-
+				case ']';
+					if ($this->get_byte() != 0) {
 						$i = $this->loop_start_position($i);
 					}
-
 				break;
 
 				// Input
-				case ',' :
-
+				case ',':
 					$this->cells[ $this->cell_pointer ] = isset( $this->input[ $this->input_pointer  ] ) ? ord( $this->input[ $this->input_pointer++ ] ) : 0;
-
 				break;
 
 			}
@@ -108,7 +85,6 @@ class BrainFuck {
 	}
 
 	private function split( $input ) {
-
 		$input = preg_split( '//', $input );
 		array_pop( $input);
 		array_shift( $input );
@@ -116,22 +92,16 @@ class BrainFuck {
 		return $input;
 	}
 
-	private function loop_start_position( $position ) {
-
+	private function loop_start_position($position) {
 		$has_prev_loops = 0;
 
-		for( $i = $position; $i >= 0; $i-- ) {
+		for ($i = $position; $i >= 0; $i--) {
+			$char = $this->chars[$i];
 
-			$char = $this->chars[ $i ];
-
-			if( $char == ']' ) {
-
+			if ($char == ']') {
 				$has_prev_loops++;
-
-			} else if ( $char == '[' ) {
-
-				if( --$has_prev_loops == 0 ) {
-
+			} else if ($char == '[') {
+				if (--$has_prev_loops == 0) {
 					return $i;
 				}
 			}
@@ -140,23 +110,17 @@ class BrainFuck {
 		throw new Exception('Cannot find loop start.');
 	}
 
-	private function loop_end_position( $position ) {
-
+	private function loop_end_position($position) {
 		$has_next_loops = 0;
 
-		for( $i = $position; $i < count( $this->chars ); $i++ ) {
-
+		for ($i = $position; $i < count($this->chars); $i++) {
 			$char = $this->chars[ $i ];
 
-			if( $char == ']' ) {
-
-				if( --$has_next_loops == 0 ) {
-
+			if ($char == ']') {
+				if (--$has_next_loops == 0) {
 					return $i;
 				}
-
-			} else if ( $char == '[' ) {
-
+			} else if ($char == '[') {
 				$has_next_loops++;
 			}
 		}
@@ -165,49 +129,36 @@ class BrainFuck {
 	}
 
 	private function get_byte() {
-
-		return $this->cells[ $this->cell_pointer ];
+		return $this->cells[$this->cell_pointer];
 	}
 
 	private function byte_to_ascii() {
-
-		return chr( $this->get_byte() );
+		return chr($this->get_byte());
 	}
 
 	private function init_cell() {
-
-		if( ! isset( $this->cells[ $this->cell_pointer ] ) )
-			$this->cells[ $this->cell_pointer ] = 0;
+		if (!isset($this->cells[$this->cell_pointer])) {
+			$this->cells[$this->cell_pointer] = 0;
+		}
 	}
 
 	private function increment_pointer() {
-
 		++$this->cell_pointer;
-
 		$this->init_cell();
 	}
 
 	private function decrement_pointer() {
-
 		--$this->cell_pointer;
-
 		$this->init_cell();
 	}
 
 	private function increment_byte() {
-
 		$this->init_cell();
-
-		$this->cells[ $this->cell_pointer ]++;
+		$this->cells[$this->cell_pointer]++;
 	}
 
 	private function decrement_byte() {
-
 		$this->init_cell();
-
 		$this->cells[ $this->cell_pointer ]--;
 	}
 }
-
-
-?>
